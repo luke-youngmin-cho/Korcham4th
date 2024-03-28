41a0df89b8a723ac0a991114b740a63aa0fae012
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2024-03-28 16:54:40 +0900

    Singleton
    - Created generic SingletonBase & SingletonMonoBase.

---
+++ MultiplayPractice/Assets/02.Scripts/Singleton/SingletonBaseOfT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MP.Singleton
+{
+    public abstract class SingletonBase<T>
+        where T : SingletonBase<T>
+    {
+        public static T instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    //ConstructorInfo constructorInfo = typeof(T).GetConstructor(new Type[] { });
+                    //constructorInfo.Invoke(null);
+
+                    s_instance = (T)Activator.CreateInstance(typeof(T));
+                }
+
+                return s_instance;
+            }
+        }
+
+        private static T s_instance;
+    }
+}
---
+++ MultiplayPractice/Assets/02.Scripts/Singleton/SingletonMonoBaseOfT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace MP.Singleton
+{
+    public abstract class SingletonMonoBase<T> : MonoBehaviour
+        where T : SingletonMonoBase<T>
+    {
+        public static T instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    s_instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                }
+
+                return s_instance;
+            }
+        }
+
+        private static T s_instance;
+    }
+}
